d5c9654ae5af9c653cd41aa704073b802d606ff6
Karen V <karenvo@usc.edu>
2019-03-26 00:16:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,45 @@ class Song
     self.class.all << self
   end
 
+  def find_or_create_by_name(name)
+
+  end
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
+  end
+
+  def self.new_by_name
+    @@all.new_by_name
+  end
+
+  def self.create_by_name
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def self.alphbetical
+
+  end
+
+  def self.new_from_filename
+
+  end
+
+  def self.create_From_filename
+
+  end
+
+  def self.destroy_all
+
+  end
+
 end
