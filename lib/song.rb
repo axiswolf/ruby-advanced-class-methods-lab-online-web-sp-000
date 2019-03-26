f1da884ed5198f950389aaf4863bde822559b5f0
Karen V <karenvo@usc.edu>
2019-03-26 00:30:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -16,24 +17,25 @@ class Song
     song
   end
 
-  def self.new_by_name(title)
+  def self.new_by_name(title) #class constructor
     song = self.new
     song.name = title
     song
   end
 
-  def self.create_by_name(title)
+  def self.create_by_name(title) #class constructor
     song = self.create
     song.name = title
     song
   end
 
-  def self.find_by_name(title) #class finders
+  def self.find_by_name(title) #class finder
     result = self.all.detect {|song| song.name == title}
     result
   end
 
   def self.find_or_create_by_name(title)
+    #either return a matching song instance with that name or create a new song with the name and return the song instance
     result = self.find_by_name(title)
     if result
       result
@@ -42,15 +44,15 @@ class Song
     end
   end
 
-  def self.alphbetical
+  def self.alphabetical
     sorted = self.all.sort_by {|song| song.name}
     sorted
   end
 
   def self.new_from_filename(filename)
-    song_array = filename.splot(" - ")
-    song_array[1] = song_array[1].chomp(" .mp3")
-    song = self.new_from_filename
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
     song.name = song_array[1]
     song.artist_name = song_array[0]
     song
@@ -58,7 +60,7 @@ class Song
 
   def self.create_from_filename(filename)
     result = self.new_from_filename(filename)
-    song = self.create_by_name
+    song = self.create
     song.name = result.name
     song.artist_name = result.artist_name
     song
@@ -67,5 +69,4 @@ class Song
   def self.destroy_all
     self.all.clear
   end
-
 end
