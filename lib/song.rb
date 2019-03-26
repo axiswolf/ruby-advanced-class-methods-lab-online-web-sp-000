7481f0cee2079e62298f777181eb8caca59f73a8
Karen V <karenvo@usc.edu>
2019-03-26 00:22:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,13 @@ class Song
     result
   end
 
-  def self.find_or_create_by_name
-
+  def self.find_or_create_by_name(title)
+    result = self.find_by_name(title)
+    if result
+      result
+    else
+      self.create_by_name(title)
+    end
   end
 
   def self.alphbetical
